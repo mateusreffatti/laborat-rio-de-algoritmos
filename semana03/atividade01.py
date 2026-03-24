d44a9c7ab001be9d2715667c6f96cf8c820ff89f
mateusreffatti <mateus.bisognin@amf.edu.br>
2026-03-24 20:29:17 -0300

    Create atividade01.py

---
+++ semana03/atividade01.py
@@ -0,0 +1,8 @@
+Salário do funcionário
+horas = float(input("Digite o total de horas trabalhadas no mês: "))
+salario = horas * 35
+
+if salario < 1000:
+   salario += 300
+
+print(f"Salário final: R$ {salario:.2f}")
