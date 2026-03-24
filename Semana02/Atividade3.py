a4d772ea66fb30b35fb075742c8b04e872d05db5
mateusreffatti <mateus.bisognin@amf.edu.br>
2026-03-24 20:49:16 -0300

    Create Atividade3.py

---
+++ Semana02/Atividade3.py
@@ -0,0 +1,6 @@
+3️⃣ Calcular idade a partir do ano de nascimento
+
+ano_nascimento = int(input("Digite o ano de nascimento: "))
+ano_atual = 2026
+idade = ano_atual - ano_nascimento
+print(f"A pessoa tem (idade) anos.")
