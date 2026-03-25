a7370d006b8a02c8ccea33157c6aab22974d95b3
mateusreffatti <mateus.bisognin@amf.edu.br>
2026-03-24 21:24:09 -0300

    Update Atividade5.py

--- Semana02/Atividade5.py
+++ Semana02/Atividade5.py
@@ -1,5 +1,3 @@
-5) Valor final de uma peça com desconto de 18%
-
 preco_venda = float(input("Digite o preço de venda da peça: R$ "))
 valor_final = preco_venda * (1 - 0.18)
 print(f"O valor final da peça com desconto é: R$ (valor_final:.2f)")
