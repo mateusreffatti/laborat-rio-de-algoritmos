ea7a3ad2022dcfd41e14e0d492a6b90fbf0c260a
mateusreffatti <mateus.bisognin@amf.edu.br>
2026-03-31 21:54:55 -0300

    Create Atividade03.py

---
+++ Semana04/Atividade03.py
@@ -0,0 +1,14 @@
+print("Qual é a distância oficial de uma maratona?")
+print("A) 21 km")
+print("B) 42,195 km")
+print("C) 10 km")
+print("D) 5 km")
+
+
+resposta = input("Digite a alternativa (A, B, C ou D): ").upper()
+
+
+if resposta == "B":
+    print("Resultado: Correto!")
+else:
+    print("Resultado: Incorreto!")
