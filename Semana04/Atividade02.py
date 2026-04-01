96f1e04075fdacb51039b03aa15a2a61eb16de93
mateusreffatti <mateus.bisognin@amf.edu.br>
2026-03-31 21:43:38 -0300

    Create Atividade02.py

---
+++ Semana04/Atividade02.py
@@ -0,0 +1,28 @@
+
+print("valor inscrição: 120")
+valor_ins = 120
+
+opção = int(input("escolha uma opção acima:"))
+
+if opção == 1:
+    print("valor corrida: 120 reais")
+    print("Pagamento à vista:)")
+    print("Pagamento em 2x ")
+    print("Pagamento em 3x ")
+    opção = int(input("escolha a opção acima:"))
+    
+elif opção == 1:
+    print("valor inscrição: 120 reais")
+    print("opção escolhida: 1")
+    
+    
+elif opção == 2:
+    print("opção escolhida: 2")
+    valor_ins = valor_ins / 2
+    print("opção escolhida: 3")
+    valor_parcela = valor_ins / 3
+
+else:
+    print("opção invalida:")
+    
+
