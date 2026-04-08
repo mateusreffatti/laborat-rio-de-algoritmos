40bcc8e320f0348c03ea3cb2255198b43f97f2db
mateusreffatti <mateus.bisognin@amf.edu.br>
2026-04-07 21:39:54 -0300

    Create Atividade2.py

---
+++ Semana05/Atividade2.py
@@ -0,0 +1,10 @@
+contador_1040 = 0
+
+while True:
+    codigo = int(input("Digite o código do brinquedo (0 para encerrar): "))
+    
+    if codigo == 0:
+        break  
+
+    if codigo == 1040:
+        contador_1040 += 1  
